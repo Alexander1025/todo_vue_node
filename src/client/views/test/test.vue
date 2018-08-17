a2d25f9b542134c0f12ed0cee6865794541d6423
756418095@qq.com <756418095@qq.com>
2018-08-17 10:57:05 +0800

    新增测试专用页面，用于测试新功能

---
+++ src/client/views/test/test.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="indexwrap">
+        <!-- <headerCommon></headerCommon> -->
+        <!-- <calendar></calendar> -->
+        <taskcomponent></taskcomponent>
+
+        <!-- <footerCommon></footerCommon> -->
+    </div>
+</template>
+
+<script>
+import headerCommon from './../../components/Header.vue';
+import calendar from './../../components/calendar.vue';
+import taskcomponent from './../../components/taskcomponent.vue';
+import footerCommon from './../../components/footer.vue';
+
+export default {
+    data () {
+        return {
+            message: ''
+        }
+    },
+    components:{
+        // headerCommon,
+        // calendar,
+        taskcomponent,
+        // footerCommon,
+    },
+    mounted:function (){
+        // let date = new Date();
+        // let mon = date.getMonth();
+        // this.$store.commit('getMonth',mon);
+
+        // this.$store.dispatch('getUserMsg');
+    }
+}
+</script>
+
+<style>
+
+</style>
