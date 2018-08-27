3d9f13703083c954cec78b79e672b7e4de25e36f
756418095@qq.com <756418095@qq.com>
2018-08-27 15:23:53 +0800

    完成首页优化，添加任务列表侧滑动画，引入popmotion.js，大小只有12k，运行流畅，可以继续使用。

--- src/client/components/taskcomponent.vue
+++ src/client/components/taskcomponent.vue
@@ -92,7 +92,7 @@ export default {
             }
             for(let i = 0 , j = this.$parent.$children ; i <= j.length-1 ; i++){
                 if(this.$parent.$children[i].$el.className == "footer_common"){
-                    this.taskheight = (document.body.clientHeight || document.documentElement.clientHeight) - this.$parent.$children[i].$el.offsetHeight - this.tasktop;
+                    this.taskheight = (document.body.clientHeight || document.documentElement.clientHeight) - this.$parent.$children[i].$el.clientHeight - this.tasktop;
                 }
             }
             this.$refs.task.style.display = "block";
@@ -116,5 +116,12 @@ export default {
         top:0;
         left: 0;
         width: 100%;
+    }
+    .tasklistwrap{
+        overflow: hidden;
+        background: -webkit-linear-gradient(left, #617FDF -20%, white 40%, white 60%, red 90%); /* Safari 5.1 - 6.0 */
+        background: -o-linear-gradient(to right, #617FDF -20%, white 40%, white 60%, red 90%); /* Opera 11.1 - 12.0 */
+        background: -moz-linear-gradient(to right, #617FDF -20%, white 40%, white 60%, red 90%); /* Firefox 3.6 - 15 */
+        background: linear-gradient(to right, #617FDF -20%, white 40%, white 60%, red 90%); /* 标准的语法 */
     }
 </style>
--- src/client/components/tasklistcomponent.vue
+++ src/client/components/tasklistcomponent.vue
@@ -1,25 +1,92 @@
 <template>
-    <div class="tasklist">
-        <div class="tasklistimg" v-if="this.item.status == 1">
-            <img src="./../static/images/icon/emptyradio.svg" alt="">
+    <div class="slidewrap" :style="{ left : slideleft + 'px' }">
+        <div class="tasklist">
+            <div class="tasklistimg" v-if="this.item.status == 1">
+                <img src="./../static/images/icon/emptyradio.svg" alt="">
+            </div>
+            <div class="tasklistimg" v-else-if="this.item.status == 2">
+                <img src="./../static/images/icon/radio.svg" alt="">
+            </div>
+
+            <div
+                class="taskdescribe"
+                :class="[status == 2 ? 'emptytext' : '']"
+                @touchstart="touchstart"
+                @touchmove="touchmove"
+                @touchend="touchend">
+                {{this.item.text}}
+            </div>
+
+            <div
+                class="tasktime">
+                {{this.item.text}}
+            </div>
         </div>
-        <div class="tasklistimg" v-else-if="this.item.status == 2">
-            <img src="./../static/images/icon/radio.svg" alt="">
+        <div class="slideright">
+            test
         </div>
-        <div class="taskdescribe" :class="[status == 2 ? 'emptytext' : '']">{{this.item.text}}</div>
-        <div class="tasktime">{{this.item.text}}</div>
     </div>
 </template>
 <script>
+import { tween, easing } from 'popmotion';
+
 export default {
     props:["item","status"],
     data () {
         return {
-
+            clientX:0,
+            slideleft:0,
+            offset:0,
+            multiple:40,
+            taskwidth:(document.documentElement.clientWidth)*0.2,
+            taskstatus: 0 // 0:初始状态,1:右侧显示,2:左侧显示,3:
         }
     },
     mounted:function (){
-        // console.log("tasklist");
+
+    },
+    methods:{
+        touchstart:function (e){
+            // console.log(e);
+            this.clientX = e.changedTouches[0].clientX;
+            // console.log("touchstart");
+        },
+        touchmove:function (e){
+            // console.log(e);
+            var translationleft = e.changedTouches[0].clientX - this.clientX + this.offset;
+            if(translationleft < 0 && translationleft >= -this.taskwidth){
+                this.slideleft = translationleft;
+                this.taskstatus = 0;
+            }else if(translationleft < -this.taskwidth){
+                var translationleft1 = e.changedTouches[0].clientX + this.taskwidth - this.clientX + this.offset;
+                this.slideleft = -this.taskwidth + (Math.atan(translationleft1/this.multiple))*this.multiple;
+                this.taskstatus = 1;
+            }else if(translationleft > 0){
+                // this.slideleft = (translationleft)/((translationleft)/80+0.8);
+                // this.slideleft = (translationleft)/(((translationleft*translationleft)/10000)+1);
+                this.slideleft = (Math.atan(translationleft/this.multiple))*this.multiple;
+                this.taskstatus = 0;
+            }
+            // console.log("touchmove");
+        },
+        touchend:function (e){
+            // console.log(e);
+            // console.log("touchend");
+            if(this.taskstatus == 0){
+                this.resilience(this.slideleft, 0);
+                this.offset = 0;
+            }else if(this.taskstatus == 1){
+                this.resilience(this.slideleft, -this.taskwidth);
+                this.offset = -this.taskwidth;
+            }
+        },
+        resilience:function (start, end){
+            tween({ from: start, to: end, duration: 400, ease: easing.backOut}).start(
+                (slideleft) => {
+                    this.slideleft = slideleft;
+                }
+            );
+        }
     }
 }
 </script>
@@ -36,9 +103,13 @@ export default {
         flex-flow: row nowrap;
         justify-content: space-between;
         align-items: center;
-        padding: 0 3%;
         font-size: 14px;
         height: 52px;
+        width: 100vw;
+        padding: 0 3%;
+        box-sizing: border-box;
+        float: left;
+        background-color: white;
     }
     .tasklistimg{
         flex: 0 1 8%;
@@ -63,5 +134,20 @@ export default {
     }
     .emptytext{
         color: #909090;
+    }
+    .slidewrap{
+        width: 120vw;
+        overflow: hidden;
+        position: relative;
+    }
+    .slideright{
+        float: left;
+        /* background-color: #FD3C2D; */
+        height: 52px;
+        line-height: 52px;
+        width: 20vw;
+        text-align: center;
+        color: white;
+        font-size: 12px;
     }
 </style>
