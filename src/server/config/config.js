97fe8145784416f93f7a36b1b8dc07d1db023fb2
756418095@qq.com <756418095@qq.com>
2018-11-24 10:32:29 +0800

    线上数据库连接

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,7 +1,8 @@
 const config = {
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'PASSroot1234',
+    // password : 'PASSroot1234',
+    password : 'zjz8161773612.',
     port: '3306',
     database: 'mytodo',
 }
