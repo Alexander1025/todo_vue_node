bafba66a58c0993c0d925a86cbe3cffafb573546
756418095@qq.com <756418095@qq.com>
2018-11-24 10:01:20 +0800

    测试线上node服务

--- src/server/index.js
+++ src/server/index.js
@@ -239,44 +239,40 @@ app.post('/node/savename', function (req, res) {
 */
 
 app.post('/node/login', function (req, res) {
-    var resdata = {};
-    resdata['data'] = "test";
-    resdata['status'] = 1;
-    res.send(resdata);
-    // var body = "";
-    // req.on('data', function (chunk) {
-    //     body += chunk;  //一定要使用+=，如果body=chunk，因为请求favicon.ico，body会等于{}
-    //     console.log("chunk:",chunk);
-    // });
-    // req.on('end', function () {
-    //     // 生成返回格式对象
-    //     let resdata = {};
-    //     // 解析参数
-    //     body = querystring.parse(body);  //将一个字符串反序列化为一个对象
-    //     console.log("body:",body);
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk;  //一定要使用+=，如果body=chunk，因为请求favicon.ico，body会等于{}
+        console.log("chunk:",chunk);
+    });
+    req.on('end', function () {
+        // 生成返回格式对象
+        let resdata = {};
+        // 解析参数
+        body = querystring.parse(body);  //将一个字符串反序列化为一个对象
+        console.log("body:",body);
 
-    //     // 业务开始
-    //     login(body).then(function (data){
-    //         console.log(data);
-    //         if(data.length > 0){
-    //             resdata['data'] = data;
-    //             resdata['status'] = 1;
-    //             res.cookie('userid', data[0]['userid'], { expires: new Date(Date.now() + 3600000), httpOnly: true });
-    //             res.send(resdata);
-    //             res.end();
-    //         }else{
-    //             resdata['data'] = data;
-    //             resdata['status'] = 0;
-    //             res.send(resdata);
-    //             res.end();
-    //         }
-    //     },function (res){
-    //         resdata['data'] = res;
-    //         resdata['status'] = 0;
-    //         res.send(resdata);
-    //         res.end();
-    //     });
-    // });
+        // 业务开始
+        login(body).then(function (data){
+            console.log(data);
+            if(data.length > 0){
+                resdata['data'] = data;
+                resdata['status'] = 1;
+                res.cookie('userid', data[0]['userid'], { expires: new Date(Date.now() + 3600000), httpOnly: true });
+                res.send(resdata);
+                res.end();
+            }else{
+                resdata['data'] = data;
+                resdata['status'] = 0;
+                res.send(resdata);
+                res.end();
+            }
+        },function (res){
+            resdata['data'] = res;
+            resdata['status'] = 0;
+            res.send(resdata);
+            res.end();
+        });
+    });
 });
 
 
