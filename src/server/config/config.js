96121850b405d74c97afff3091e1db5455de49ec
756418095@qq.com <756418095@qq.com>
2018-11-26 10:01:59 +0800

    分开配置生产环境和开发环境

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,14 +1,25 @@
-const config = {
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : 'PASSroot1234',
-    // password : 'zjz8161773612.',
-    port: '3306',
-    database: 'mytodo',
+if(process.env.NODE_ENV ==  'production'){
+    var host = '127.0.0.1';
+    var user = 'root';
+    var password = 'zjz8161773612';
+    var port = '3306';
+    var database = 'mytodo';
+}else{
+    var host = '127.0.0.1';
+    var user = 'root';
+    var password = 'PASSroot1234';
+    var port = '3306';
+    var database = 'mytodo';
 }
 
 
-
+const config = {
+    host     : host,
+    user     : user,
+    password : password,
+    port: port,
+    database: database,
+}
 
 exports.config = config;
 // export {
