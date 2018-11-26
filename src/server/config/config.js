3882796b79a951d506409fcd89968e26a1de1e85
756418095@qq.com <756418095@qq.com>
2018-11-26 10:29:44 +0800

    分开配置生产环境和开发环境

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV ==  'production'){
     config = {
         host     : '127.0.0.1',
         user     : 'root',
-        password : 'zjz8161773612',
+        password : 'zjz8161773612.',   //记得最后有点！！！
         port     : '3306',
         database : 'mytodo',
     }
