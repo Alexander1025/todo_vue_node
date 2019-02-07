e3c6ae86aa21f97e666abe0cdc97e0756637b879
Alexander1025 <756418095@qq.com>
2019-02-07 11:36:38 +0800

    fix 优化添加主题页面

--- src/client/views/setting/addtheme.vue
+++ src/client/views/setting/addtheme.vue
@@ -1,22 +1,32 @@
 <template>
-    <div class="addtheme">
+    <div class="addtheme addthemewarp">
         <theme
-            v-for="(item, index) in themelist"
-            :themeid="item.themeid"
-            :title="item.title"
-            :thisstyle="item.thisstyle"
-            :thisheadstyle="item.thisheadstyle"
-            :present="item.present"
-            :imgsrc="item.imgsrc"
-            :item="item"
-            :key="index"
+            :themeid="themeid"
+            :title="title"
+            :thisstyle="thisstyle"
+            :thisheadstyle="thisheadstyle"
+            :present="present"
+            :imgsrc="imgsrc"
         >
         </theme>
         <div class="link"></div>
         <div class="centerwrap">
-            <input type="text" class="logininput">
+            <input type="text" class="logininput" placeholder="给主题起个名字吧 ^_^">
         </div>
-        <colorPicker v-model="color" defaultColor="#ff0000"></colorPicker>
+        <div class="color">
+            <span>头部主题颜色</span>
+            <colorPicker v-model="color" defaultColor="#ff0000"></colorPicker>
+        </div>
+        <div class="color">
+            <span>主题颜色</span>
+            <colorPicker v-model="colormain" defaultColor="#ff0000"></colorPicker>
+        </div>
+        <div
+            class="submit"
+            @click=""
+        >
+            新增
+        </div>
     </div>
 </template>
 <script>
@@ -25,17 +35,16 @@ import colorPicker from './../../components/colorpicker.vue'
 export default {
     data() {
         return{
-            themelist: [
-                {
-                    themeid:1,
-                    title: "自定义主题",
-                    thisstyle: "background: linear-gradient(-45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);",
-                    thisheadstyle:"background: #FD7897;",
-                    present: false,
-                    imgsrc:"",
-                },
-            ],
-            color:''
+
+            themeid:1,
+            title: "自定义主题",
+            thisstyle: "background: linear-gradient(-45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);",
+            thisheadstyle:"background: #FD7897;",
+            present: false,
+            imgsrc:"",
+
+            color:'',
+            colormain:'',
         }
     },
     components:{
@@ -57,7 +66,7 @@ export default {
             props:["themeid","title","thisstyle","thisheadstyle","present","imgsrc","item"],
             methods:{
                 imgError: function (item) {
-                    item.imgsrc = require('./../../static/images/icon/check.svg');
+                    item = require('./../../static/images/icon/check.svg');
                 },
             }
 
@@ -69,11 +78,37 @@ export default {
 }
 </script>
 <style>
+    .submit{
+        display: block;
+        width: 80%;
+        position: fixed;
+        left: 10%;
+        bottom: 60px;
+        line-height: 42px;
+        background-color: #0084c6;
+        color: white;
+        text-align: center;
+        text-decoration: none;
+        margin: 80px auto 0;
+        border-radius: 3px;
+    }
+    .color{
+        margin-top: 20px;
+    }
+    .m-colorPicker, .colorBtn, .colorBtn, #task{
+        position: relative;
+        display: block !important;
+        width: 80vw !important;
+        height: 40px !important;
+        margin: 0px auto 0;
+        border: none;
+        border-radius: 3px;
+    }
     .addtheme .logininput{
         text-align: center;
         border: none;
         border-bottom: 1px solid #e2e2e2;
-        width: 100%;
+        width: 80%;
         line-height: 50px;
         margin: 10px 0;
         font-size: 16px;
@@ -119,7 +154,7 @@ export default {
         height: 6vw;
         position: absolute;
     }
-    .theme span{
+    .theme span, .color span{
         display: block;
         font-size: 10px;
         line-height: 26px;
